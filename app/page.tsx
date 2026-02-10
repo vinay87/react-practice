ffa299208c62dc566de8536eb4905f4a316285f0
Vinay <vinaykphp@gmail.com>
2026-02-10 15:33:13 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Click to increment</button> &nbsp;
+      <button onClick={() => setCount(count - 1)}> Click to decrement</button>
+
+    </>
+
+
+  );
+}
